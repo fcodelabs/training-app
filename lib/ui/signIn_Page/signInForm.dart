4e58a371a491cdf37dbcc70f0cd31f2975c1a58d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-06 14:39:16 +0530

    Add Sign_In form

---
+++ lib/ui/signIn_Page/signInForm.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[100],
+        elevation: 0,
+      ),
+      backgroundColor: Colors.blue[100],
+      body: Padding(padding: EdgeInsets.all(12.0)),
+    );
+  }
+}
