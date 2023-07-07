a4ccfda23eac51a867cf440a9fa4a5cae44d7e13
Sandun Induranga <sandutharu40@gmail.com>
2023-07-07 10:51:56 +0530

    Added New Page Diary Home Page

---
+++ lib/ui/diary_home_page/diary_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiaryHomePage extends StatelessWidget {
+  const DiaryHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
