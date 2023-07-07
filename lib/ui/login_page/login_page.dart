bb51bfe941d9b5cb28dc071f9767688da03e5b36
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-07-07 09:58:50 +0530

    Background Image Set

--- lib/ui/login_page/login_page.dart
+++ lib/ui/login_page/login_page.dart
@@ -1,10 +1,24 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('../assets/img/background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
