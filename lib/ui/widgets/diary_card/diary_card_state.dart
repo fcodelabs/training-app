0ebde59c75e96485b5e4ca2045b3d54c90d895e3
Sandun Induranga <sandutharu40@gmail.com>
2023-07-11 01:59:33 +0530

    Diary Card States Refactored

--- lib/ui/widgets/diary_card/diary_card_state.dart
+++ lib/ui/widgets/diary_card/diary_card_state.dart
@@ -1,15 +1,11 @@
 class DiaryCardState {
-  final bool showMore;
   final bool isPressed;
 
-  DiaryCardState({required this.showMore, required this.isPressed});
+  DiaryCardState({required this.isPressed});
 
-  static DiaryCardState get initialState =>
-      DiaryCardState(showMore: false, isPressed: false);
+  static DiaryCardState get initialState => DiaryCardState(isPressed: false);
 
-  DiaryCardState clone({bool? showMore, bool? isPressed}) {
-    return DiaryCardState(
-        showMore: showMore ?? this.showMore,
-        isPressed: isPressed ?? this.isPressed);
+  DiaryCardState clone({bool? isPressed}) {
+    return DiaryCardState(isPressed: isPressed ?? this.isPressed);
   }
 }
