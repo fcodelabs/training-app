31659d9234bb467dda31ba119a278915af7a8b46
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-07-11 10:51:27 +0530

    Refactor login page name

--- lib/ui/root_page/root_page.dart
+++ lib/ui/root_page/root_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:training_app/ui/login_page/login_page.dart';
+import 'package:training_app/ui/login_page/login_page_view.dart';
 
 void main() {
   runApp(const DearDiaryApp());
