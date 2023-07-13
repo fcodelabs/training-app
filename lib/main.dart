1c7b75c9d20527d70cf1321de02c43d7839fd5f8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-13 08:09:54 +0530

    Add firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'ui/root_page/root_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await FirebaseConfig.initialize();
   await Firebase.initializeApp();
   runApp(const DearDiaryApp());
 }
